566c850ce9285cbdd049c8d10cb36d1da0045ca5
ItSolution21 <161233126+ItSolution21@users.noreply.github.com>
2024-05-02 15:59:24 +0530

    Add files via upload

---
+++ task5.js
@@ -0,0 +1,41 @@
+
+//operators - which are helpful or responsible for making an operations
+    // operation can be of many types like arithmetic, assignment, comparison, logical, bitwise, etc.
+
+    // Arithmetic operators
+    let a = 9
+    let b = 7
+
+    console.log(a + b) // 16
+    console.log(a - b) //2
+    console.log(a * b) //63
+    console.log(a / b) //1.3333333333333333
+    console.log(b % a) 
+    console.log(5 ** 2) 
+
+    // Assignment operators : storing and manipulating at the same time 
+
+    let x = 9;
+    let y = 3; 
+    
+    x += y;
+    x *=y;
+    x %= y
+    // x = x %y
+    // x = x *y => 
+    // x = x + y => 9 + 3 => 12 
+    console.log(x, y)
+
+
+    // Comparison operators 
+    // comparing two values and returning a boolean value
+
+    let u = 9;
+    let v = '9';
+
+    console.log(u == v)
+
+    console.log(u === v)
+    console.log( u!= v)
+    console.log(u !== v)
+
